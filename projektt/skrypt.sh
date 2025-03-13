7a6b9fd615703f565c21bf940793a345dabc850a
Rafał <98359882+Mefir01@users.noreply.github.com>
2025-03-13 10:05:21 +0100

    Update skrypt.sh

--- projektt/skrypt.sh
+++ projektt/skrypt.sh
@@ -1,19 +1,19 @@
-curl -X POST "http://127.0.0.1:8000/coffees" -H "Content-Type: application/json" -d '{
+curl -X POST "http://127.0.0.1:8080/coffees" -H "Content-Type: application/json" -d '{
   "id": 1,
   "name": "Espresso",
   "description": "Strong and black coffee",
   "price": 5.99
 }'
 
-curl -X GET "http://127.0.0.1:8000/coffees" -H "Accept: application/json"
+curl -X GET "http://127.0.0.1:8080/coffees" -H "Accept: application/json"
 
-curl -X GET "http://127.0.0.1:8000/coffees/1" -H "Accept: application/json"
+curl -X GET "http://127.0.0.1:8080/coffees/1" -H "Accept: application/json"
 
-curl -X PUT "http://127.0.0.1:8000/coffees/1" -H "Content-Type: application/json" -d '{
+curl -X PUT "http://127.0.0.1:8080/coffees/1" -H "Content-Type: application/json" -d '{
   "id": 1,
   "name": "Double Espresso",
   "description": "Stronger coffee",
   "price": 6.99
 }'
 
-curl -X DELETE "http://127.0.0.1:8000/coffees/1"
+curl -X DELETE "http://127.0.0.1:8080/coffees/1"
